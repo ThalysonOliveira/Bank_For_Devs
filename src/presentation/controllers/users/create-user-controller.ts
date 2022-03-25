f0d9c16d8bb0fd8044e1b0d7d677f5553460507e
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-03-25 16:58:30 -0300

    feat: ensure return 500 if EmailValidator throws

--- src/presentation/controllers/users/create-user-controller.ts
+++ src/presentation/controllers/users/create-user-controller.ts
@@ -7,20 +7,29 @@ class CreateUserController implements Controller {
   constructor (private emailValidator:EmailValidator) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['name', 'email', 'cpfCnpj', 'password']
+    try {
+      const requiredFields = ['name', 'email', 'cpfCnpj', 'password']
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(`Missing param: ${field}`)
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(`Missing param: ${field}`)
+        }
       }
-    }
 
-    const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
+      const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
 
-    if (!emailIsValid) return badRequest('Invalid email')
+      if (!emailIsValid) return badRequest('Invalid email')
 
-    return {
-      statusCode: 201
+      return {
+        statusCode: 201
+      }
+    } catch (error) {
+      return ({
+        statusCode: 500,
+        body: {
+          message: 'Internal Error Server'
+        }
+      })
     }
   }
 }
