d121782133c0b82633eda856528c0403a97acb5f
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-03-25 14:27:24 -0300

    feat: ensure return 400 if no email is provided

--- src/presentation/controllers/users/create-user-controller.ts
+++ src/presentation/controllers/users/create-user-controller.ts
@@ -6,6 +6,13 @@ class CreateUserController {
         message: 'Missing param: name'
       }
     }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        message: 'Missing param: email'
+      }
+    }
   }
 }
 
