df28a1bec9d1edd3cd584e08ac80a9fb8d07f74c
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-03-26 17:46:16 -0300

    test: ensure return 500 if CnpjValidator throws

--- src/presentation/controllers/users/create-user-controller.spec.ts
+++ src/presentation/controllers/users/create-user-controller.spec.ts
@@ -364,4 +364,24 @@ describe('Create User Controller', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body.message).toBe('Internal Error Server')
   })
+
+  test('Should return 500 if CnpjValidator throws', async () => {
+    const { sut, cnpjValidatorStub } = makeSut()
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        cpfCnpj: 'any_cpfCnpj',
+        password: 'any_password'
+      }
+    }
+
+    jest.spyOn(cnpjValidatorStub, 'isValid').mockImplementationOnce(() => { throw new Error() })
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body.message).toBe('Internal Error Server')
+  })
 })
