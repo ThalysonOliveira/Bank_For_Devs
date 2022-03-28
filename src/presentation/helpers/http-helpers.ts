c17915fab543d34b559ed0fa721438ca83876fc1
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-03-28 10:58:07 -0300

    refactor: change the syntax of the http response return

--- src/presentation/helpers/http-helpers.ts
+++ src/presentation/helpers/http-helpers.ts
@@ -1,34 +1,30 @@
 import { HttpResponse } from '../protocols/http'
 
-const badRequest = (message: string, data?: unknown): HttpResponse => {
-  return ({
-    statusCode: 400,
-    body: {
-      success: false,
-      message,
-      data
-    }
-  })
-}
+const badRequest = (message: string, data?: unknown): HttpResponse => ({
+  statusCode: 400,
+  body: {
+    success: false,
+    message,
+    data
+  }
+})
 
-const created = (message?: string): HttpResponse => {
-  return ({
-    statusCode: 201,
-    body: {
-      message
-    }
-  })
-}
+const created = (message?: string): HttpResponse => ({
+  statusCode: 201,
+  body: {
+    message
+  }
+})
 
-const serverError = (error?: Error):HttpResponse => {
+const serverError = (error?: Error): HttpResponse => {
   console.log(error?.stack)
 
-  return ({
+  return {
     statusCode: 500,
     body: {
       message: 'Internal error server'
     }
-  })
+  }
 }
 
 export { badRequest, serverError, created }
