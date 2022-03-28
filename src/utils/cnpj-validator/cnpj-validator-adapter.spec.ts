50d61fa82ba9ef35339fb65cba2ec787327b6038
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-03-28 16:49:33 -0300

    test: ensure CnpjValidatorAdapter return true if cpf-cnpj-validator return true

--- src/utils/cnpj-validator/cnpj-validator-adapter.spec.ts
+++ src/utils/cnpj-validator/cnpj-validator-adapter.spec.ts
@@ -1,11 +1,30 @@
 import { CnpjValidatorAdapter } from './cnpj-validator-adapter'
+import { cnpj } from 'cpf-cnpj-validator'
+
+jest.mock('cpf-cnpj-validator', () => ({
+  cnpj: {
+    isValid ():boolean {
+      return true
+    }
+  }
+}))
 
 describe('Cnpj Validator Adapter', () => {
   test('Should return false if cpf-cnpj-validator return false', () => {
     const sut = new CnpjValidatorAdapter()
 
+    jest.spyOn(cnpj, 'isValid').mockReturnValueOnce(false)
+
     const isValid = sut.isValid('invalid_cnpj')
 
     expect(isValid).toBe(false)
+  })
+
+  test('Should return true if cpf-cnpj-validator return true', () => {
+    const sut = new CnpjValidatorAdapter()
+
+    const isValid = sut.isValid('valid_cnpj')
+
+    expect(isValid).toBe(true)
   })
 })
