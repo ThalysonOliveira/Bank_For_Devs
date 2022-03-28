868c948f754d4dc4d52d8b7dda5a43de63a75a85
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-03-28 17:00:17 -0300

    refactor: move sut to factory helper method

--- src/utils/cpf-validator/cpf-validator-adapter.spec.ts
+++ src/utils/cpf-validator/cpf-validator-adapter.spec.ts
@@ -9,32 +9,34 @@ jest.mock('cpf-cnpj-validator', () => ({
   }
 }))
 
+const makeSut = (): CpfValidatorAdapter => new CpfValidatorAdapter()
+
 describe('Cpf Validator Adapter', () => {
   test('Should return false if cpf-cnpj-validator return false', () => {
-    const sut = new CpfValidatorAdapter()
+    const sut = makeSut()
 
     jest.spyOn(cpf, 'isValid').mockReturnValueOnce(false)
 
-    const isValid = sut.isValid('any_cpfCnpj')
+    const isValid = sut.isValid('invalid_cpf')
 
     expect(isValid).toBe(false)
   })
 
   test('Should return true is cpf-cnpj-validator return true', () => {
-    const sut = new CpfValidatorAdapter()
+    const sut = makeSut()
 
-    const isValid = sut.isValid('any_cpfCnpj')
+    const isValid = sut.isValid('invalid_cpf')
 
     expect(isValid).toBe(true)
   })
 
   test('Should CpfValidatorAdapter call cpf-cnpj-validator with correct value', () => {
-    const sut = new CpfValidatorAdapter()
+    const sut = makeSut()
 
     const cpfCnpjValidatorSpy = jest.spyOn(cpf, 'isValid')
 
-    sut.isValid('any_cpfCnp')
+    sut.isValid('any_cpf')
 
-    expect(cpfCnpjValidatorSpy).toHaveBeenCalledWith('any_cpfCnp')
+    expect(cpfCnpjValidatorSpy).toHaveBeenCalledWith('any_cpf')
   })
 })
