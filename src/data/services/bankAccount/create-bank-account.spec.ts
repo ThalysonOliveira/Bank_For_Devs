53093bb8be1d43821fe75f84e8f3f24846994d7e
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-03-29 15:50:46 -0300

    refactor: move sut to factory method helper

--- src/data/services/bankAccount/create-bank-account.spec.ts
+++ src/data/services/bankAccount/create-bank-account.spec.ts
@@ -2,27 +2,41 @@ import { BankAccountEntity } from '../../entities/bank-account'
 import { CreateBankAccountRepository } from '../../repositories/bankAccount/create-bank-account-repository'
 import { CreateBankAccountService } from './create-bank-account-service'
 
+type SutTypes = {
+  sut: CreateBankAccountService,
+  createBankAccountRepositoryStub: CreateBankAccountRepository
+}
+
+const makeCreateBankAccountRepositoryStub = (): CreateBankAccountRepository => {
+  class CreateBankAccountRepositoryStub implements CreateBankAccountRepository {
+    async execute (): Promise<BankAccountEntity> {
+      const fakeBankAccount = {
+        id: 1,
+        name: 'radon_name',
+        type: 'any_type',
+        agency: 1,
+        number: 1,
+        balance: 0,
+        created_at: new Date()
+      }
+
+      return new Promise(resolve => resolve(fakeBankAccount))
+    }
+  }
+  return new CreateBankAccountRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const createBankAccountRepositoryStub = makeCreateBankAccountRepositoryStub()
+
+  const sut = new CreateBankAccountService(createBankAccountRepositoryStub)
+
+  return { sut, createBankAccountRepositoryStub }
+}
+
 describe('Create Bank Account', () => {
   test('Should call CreateBankAccountRepository with successfully', async () => {
-    class CreateBankAccountRepositoryStub implements CreateBankAccountRepository {
-      async execute (): Promise<BankAccountEntity> {
-        const fakeBankAccount = {
-          id: 1,
-          name: 'radon_name',
-          type: 'any_type',
-          agency: 1,
-          number: 1,
-          balance: 0,
-          created_at: new Date()
-        }
-
-        return new Promise(resolve => resolve(fakeBankAccount))
-      }
-    }
-
-    const createBankAccountRepositoryStub = new CreateBankAccountRepositoryStub()
-
-    const sut = new CreateBankAccountService(createBankAccountRepositoryStub)
+    const { sut, createBankAccountRepositoryStub } = makeSut()
 
     const createBankAccountRepositoryStubSpy = jest.spyOn(createBankAccountRepositoryStub, 'execute')
 
