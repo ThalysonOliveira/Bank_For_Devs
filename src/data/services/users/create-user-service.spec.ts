ab43ec0e964be3cb6b4c981a71dd9edb5a574200
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-03-29 17:39:27 -0300

    refactor: move sut to factory method helper

--- src/data/services/users/create-user-service.spec.ts
+++ src/data/services/users/create-user-service.spec.ts
@@ -1,16 +1,32 @@
 import { Encrypter } from '../../protocols/encrypter'
 import { CreateUserService } from './create-user-service'
 
-describe('Create User Service', () => {
-  test('Should call Encrypter with correct password', async () => {
-    class EncrypterStub implements Encrypter {
-      async encrypt (): Promise<string> {
-        return new Promise(resolve => resolve('hashed_password'))
-      }
-    }
-    const encrypterStub = new EncrypterStub()
-    const sut = new CreateUserService(encrypterStub)
+type SutTypes = {
+  sut: CreateUserService
+  encrypterStub: Encrypter
+}
 
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt (): Promise<string> {
+      return new Promise(resolve => resolve('hashed_password'))
+    }
+  }
+  return new EncrypterStub()
+}
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncrypter()
+
+  const sut = new CreateUserService(encrypterStub)
+
+  return { sut, encrypterStub }
+}
+
+describe('Create User Service', () => {
+  const { sut, encrypterStub } = makeSut()
+
+  test('Should call Encrypter with correct password', async () => {
     const userData = {
       name: 'any_name',
       email: 'any_email',
